f1a588393107fd00c9f81845038450f9ce8f34bd
Miguel Angel <michirapper2001@gmail.com>
2020-10-12 11:51:09 +0200

    Rotate bug

--- app/src/main/java/com/example/calculadoramiguelangel/MainActivity2.kt
+++ app/src/main/java/com/example/calculadoramiguelangel/MainActivity2.kt
@@ -2,6 +2,9 @@ package com.example.calculadoramiguelangel
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -12,6 +15,27 @@ class MainActivity2 : AppCompatActivity() {
         val objetoIntent : Intent = intent
         var user = objetoIntent.getStringExtra("user")
         setTitle("Holita " + user);
+    }
+    fun onClick(v: View) {
+        var calculo = findViewById<TextView>(R.id.textView)
+        val b: Button = v as Button
+        val buttonText: String = b.getText().toString()
+        print(buttonText)
+        val caluloAnterior = calculo.text
+        calculo.text = caluloAnterior.toString() + buttonText
+    }
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        var calculo = findViewById<TextView>(R.id.textView)
+        savedInstanceState.putString("MyString", calculo.text.toString())
+
+        super.onSaveInstanceState(savedInstanceState)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        var calculo = findViewById<TextView>(R.id.textView)
+
+        super.onRestoreInstanceState(savedInstanceState)
+        val myString = savedInstanceState.getString("MyString")
+        calculo.text = myString
     }
 
 
